06601c3814509566965d140bcf5b8f401c11b1a7
kuldeep badhan <kuldeepbadhan781@gmail.com>
2023-02-21 11:48:26 +0530

    make touchable slider on home with swiper js

--- src/components/Card2/Card2.jsx
+++ src/components/Card2/Card2.jsx
@@ -1,48 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
 import "./Card2.scss";
-const Card = ({ imagesArray, translateX, cardLoc, cardSetter }) => {
+const ImageSlider = ({ imagesArray, cardLoc, cardSetter }) => {
   return (
-    <Link to={"#"} className="card2 custom__card2">
-      <div className="corousel">
+    <div className="image__slider custom__card2">
+      <Swiper
+        style={{
+          height: "100%",
+        }}
+        modules={[Pagination]}
+        spaceBetween={0}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}
+      >
         {imagesArray.map((item) => {
           return (
-            <div
-              className="corousel__card"
-              style={{
-                transform: `${translateX}`,
-              }}
-            >
-              <Link to={item.url}>
-                <div className="details">
-                  <img src={item.imgSrc} alt="image" />
-                  <h2>{item.heading.substring(0, 40) + "..."}</h2>
-                  <small>{item.tag}</small>
-                  <p>{item.desc.substring(0, 80) + "..."}</p>
-                  <div className="hover_transition"></div>
-                </div>
+            <SwiperSlide>
+              <Link
+                to={item.url}
+                className="slide"
+                style={{
+                  backgroundImage: `url(${item.imgSrc})`,
+                }}
+              >
+                <h2>{item.heading.substring(0, 40) + "..."}</h2>
+                <small>{item.tag}</small>
+                <p>{item.desc.substring(0, 80) + "..."}</p>
+                <div className="hover_transition"></div>
               </Link>
-            </div>
+            </SwiperSlide>
           );
         })}
-      </div>
-
-      <div className="cardsbtnContainer">
-        <span
-          className={cardLoc == 0 ? "cardsbtn active" : "cardsbtn"}
-          onClick={() => cardSetter(0)}
-        ></span>
-        <span
-          className={cardLoc == 1 ? "cardsbtn active" : "cardsbtn"}
-          onClick={() => cardSetter(1)}
-        ></span>
-        <span
-          className={cardLoc == 2 ? "cardsbtn active" : "cardsbtn"}
-          onClick={() => cardSetter(2)}
-        ></span>
-      </div>
-    </Link>
+      </Swiper>
+    </div>
   );
 };
 
-export default Card;
+export default ImageSlider;
